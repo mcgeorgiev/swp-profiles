11a0f4bb5bf4710104addb95ec3ee66d88b50b75
mcgeorgiev <michaelcgeorgiev@protonmail.com>
2018-11-13 21:56:34 +0000

    Swap profiles

--- swap_profile.sh
+++ swap_profile.sh
@@ -4,19 +4,9 @@ OLD_PROFILE=$PROFILE
 
 PROFILES=profiles
 
-
-for file in $(find ${PROFILES} -mindepth 1) 
-do
-  if [ ! -f $file ]; then
-    echo "Profile not found!"
-    exit 1
-  fi
-  # echo "$(basename "$file")"
-done
-
 if [[ -e "profiles/$NEW_PROFILE" ]]; then
   echo "Swapping $OLD_PROFILE for $NEW_PROFILE"
-  echo "PROFILE=${NEW_PROFILE}" > swap_profile
+  echo "PROFILE=~/swp_profiles/profiles/${NEW_PROFILE}" > swap_profile
 else 
   echo "Profile not found"
   exit 1
